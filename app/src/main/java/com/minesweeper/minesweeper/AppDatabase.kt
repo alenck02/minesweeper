8797d0a4b04bfa72836745306854a17571eb0501
alenck02 <wjdgkdusjeong@naver.com>
2024-01-19 14:53:21 +0900

    fifth commit

--- app/src/main/java/com/minesweeper/minesweeper/AppDatabase.kt
+++ app/src/main/java/com/minesweeper/minesweeper/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.*
 import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
 
-@Database(entities = [MineSweeperEntity::class], version = 2)
+@Database(entities = [MineSweeperEntity::class], version = 3)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun mineSweeperDao(): MineSweeperDao
@@ -21,7 +21,7 @@ abstract class AppDatabase : RoomDatabase() {
                     AppDatabase::class.java,
                     "app_database"
                 )
-                    .addMigrations(MIGRATION_1_2)
+                    .addMigrations(MIGRATION_1_2, MIGRATION_2_3)
                     .build()
 
                 INSTANCE = instance
@@ -53,6 +53,33 @@ abstract class AppDatabase : RoomDatabase() {
         """.trimIndent())
                 database.execSQL("DROP TABLE mine_sweeper_table")
                 database.execSQL("ALTER TABLE new_mine_sweeper_table RENAME TO mine_sweeper_table")
+            }
+        }
+
+        private val MIGRATION_2_3 = object : Migration(2, 3) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("""
+            CREATE TABLE mine_sweeper_table2 (
+                id INTEGER PRIMARY KEY NOT NULL,
+                difficulty INTEGER NOT NULL DEFAULT 1,
+                elapsedTime INTEGER NOT NULL,
+                MineCount INTEGER NOT NULL,
+                nonMineButtonClickedCount INTEGER NOT NULL,
+                nonMineButtonCount INTEGER NOT NULL,
+                minePositions TEXT NOT NULL,
+                cellOpened TEXT NOT NULL,
+                cellFlagged TEXT NOT NULL,
+                cellMineCountNearby TEXT NOT NULL
+            )
+        """.trimIndent())
+                database.execSQL("""
+            INSERT INTO mine_sweeper_table2 (id, elapsedTime, MineCount, nonMineButtonClickedCount,
+            nonMineButtonCount, minePositions, cellOpened, cellFlagged, cellMineCountNearby)
+            SELECT id, elapsedTime, MineCount, nonMineButtonClickedCount, nonMineButtonCount, minePositions, cellOpened,
+            cellFlagged, cellMineCountNearby FROM mine_sweeper_table
+        """.trimIndent())
+                database.execSQL("DROP TABLE mine_sweeper_table")
+                database.execSQL("ALTER TABLE mine_sweeper_table2 RENAME TO mine_sweeper_table")
             }
         }
     }
